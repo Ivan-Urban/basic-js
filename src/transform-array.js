3e3cf37239e8abce479b4ad9581a132c3ce519d5
Ivan-Urban <ivan.urbanovich.00@gmail.com>
2022-04-30 23:27:28 +0300

    feat: make transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,42 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  //throw new NotImplementedError('Not implemented');
+  if (!Array.isArray(arr)) throw new Error("'arr' parameter must be an instance of the Array!");
+  let countOfConstrols = 0;
+  const arrOfControls = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+  arr.forEach((el, index) => {
+    if (arrOfControls.includes(el)) {
+      countOfConstrols++
+    }
+  })
+  if (countOfConstrols == 0) return arr
+
+  const resultArr = arr.map((el, index) => {
+    if (arr[index - 1] == '--discard-next') return undefined;
+    if (arr[index + 1] == '--discard-prev') return undefined;
+    if (el == '--double-next') return arr[index + 1];
+    if ((el == '--double-prev') && (arr[index-2] != '--discard-next')) return arr[index - 1];
+    return el
+  }).filter(el => {
+    if ((!arrOfControls.includes(el) && (el !== undefined))) return el 
+  })
+
+  return resultArr
 }
+
+// const arr = [1, 2, 3, '--double-next', 1337, '--discard-prev', 4, 5];
+
+// const resultArr = arr.map((el, index) => {
+//   if (arr[index - 1] == '--discard-next') return undefined;
+//   if (arr[index + 1] == '--discard-prev') return undefined;
+//   if (el == '--double-next') return arr[index + 1];
+//   if ((el == '--double-prev') && (arr[index-2] != '--discard-next')) return arr[index - 1];
+//   return el
+// })
+
+// console.log(resultArr);
 
 module.exports = {
   transform
