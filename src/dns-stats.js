a6ef326f51e233db2315977d3c4374ad84e0b668
Ivan-Urban <ivan.urbanovich.00@gmail.com>
2022-05-02 00:28:31 +0300

    feat: dns-stats

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,40 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  // throw new NotImplementedError('Not implemented');
+  const objOfDom = {};
+  
+  domains.forEach(el => {
+    let arrOfDom = el.split('.').reverse().map(el => {
+      return '.' + el;
+    });
+    for (let i = 0; i < arrOfDom.length; i++) {
+      let keyName = '';
+      for (let j = 0; j <= i; j++) {
+        keyName +=arrOfDom[j];
+      }
+      if (objOfDom[keyName] == undefined) {
+        objOfDom[keyName] = 0;
+      }
+    }
+  });
+
+  for (let key in objOfDom) {
+    let str = '';
+    if (key.split('.').length == 2) {
+      str = key;
+    } else {
+      str = key.split('.').reverse().join('.').slice(0, key.split('.').reverse().join('.').length-1);
+    }
+    domains.forEach(el => {
+      if (el.includes(str)) {
+        objOfDom[key]++;
+      }
+    });
+  }
+
+  return objOfDom;
 }
 
 module.exports = {
